ac878cfd78c7b9a6488c194e59f0bd6a3355640d
jaxvall <ern.jaxv@gmail.com>
2019-12-26 02:11:39 +0100

    fsfsd

--- CaptoApplication/CaptoApplication.iOS/AppDelegate.cs
+++ CaptoApplication/CaptoApplication.iOS/AppDelegate.cs
@@ -4,6 +4,8 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
+using Plugin.LocalNotifications;
+using UserNotifications;
 
 namespace CaptoApplication.iOS
 {
@@ -22,6 +24,23 @@ namespace CaptoApplication.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                // Ask the user for permission to get notifications on iOS 10.0+
+                UNUserNotificationCenter.Current.RequestAuthorization(
+                        UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound,
+                        (approved, error) => { });
+            }
+            else if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                // Ask the user for permission to get notifications on iOS 8.0+
+                var settings = UIUserNotificationSettings.GetSettingsForTypes(
+                        UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound,
+                        new NSSet());
+
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
+
             Rg.Plugins.Popup.Popup.Init();
             global::Xamarin.Forms.Forms.Init();
             ZXing.Net.Mobile.Forms.iOS.Platform.Init();
